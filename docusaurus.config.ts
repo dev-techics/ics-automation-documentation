387cc01371b644906097b0e204cae7db23a63a9e
Dwip Sarker <email.dwip@gmail.com>
2025-12-11 16:41:00 +0600

    build added

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,9 @@ const config: Config = {
   organizationName: "facebook", // Usually your GitHub org/user name.
   projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+
+  onBrokenLinks: 'warn', // or 'ignore'
+  onBrokenMarkdownLinks: 'warn', // or 'ignore'
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
